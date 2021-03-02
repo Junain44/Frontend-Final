8ee9d57e7486b283edc7902f75512635ca35a4fb
junain44 <junaingallie44@gmail.com>
2021-03-02 16:19:51 +0200

    first commit

---
+++ assets/script/register.js
@@ -0,0 +1,21 @@
+function createUsers(){
+    const inputs = document.getElementsByTagName("input");
+
+    fetch("http://127.0.0.1:5000/add_user/",{
+        method: 'POST',
+        body: JSON.stringify({
+            first_name: inputs[0].value, 
+            last_name: inputs[1].value,
+            password: inputs[2].value,
+            confirm: inputs[3].value,
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    })                            
+    .then((response) => response.json())
+    .then((json) => {
+        alert("User has been created");
+        document.getElementById("register").reset();
+    });
+}
