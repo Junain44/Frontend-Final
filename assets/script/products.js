35f1661ce98184adf8b71165a5fbba4982a1f00d
junain44 <junaingallie44@gmail.com>
2021-03-08 16:19:43 +0200

    frontend changes

---
+++ assets/script/products.js
@@ -0,0 +1,26 @@
+function getProd() {
+    fetch("http://127.0.0.1:5000//show_products/")
+    .then((response) => response.json())
+    .then((data) => {
+        console.table(data);
+        let list = document.getElementById('products');
+        console.log(list);
+        data.forEach((product) => {
+            let item =`
+            <div class ="product-con">
+                <div class="product-image">
+                    <img src=${product.image}/>
+                </div>
+                <div class="product-info">
+                <h4>${product.name}</h4>
+                <h4>${product.description}</h4>
+                <h4>${product.price}</h4>
+                <button class="btn" type="submit">Add to cart</button>
+                </div>
+            </div>
+            `;
+            list.innerHTML += item;
+            });
+        });
+}
+getProd();
