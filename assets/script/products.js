0e0df74a8b2546c3a6ddbc2dca3a7a8a2871955d
Junain44 <71064108+Junain44@users.noreply.github.com>
2021-03-16 10:10:33 +0200

    Update products.js

--- assets/script/products.js
+++ assets/script/products.js
@@ -1,5 +1,5 @@
 function getProd() {
-    fetch("http://127.0.0.1:5000//show_products/")
+    fetch("https://glacial-sierra-17146.herokuapp.com/show_products/")
     .then((response) => response.json())
     .then((data) => {
         console.table(data);
